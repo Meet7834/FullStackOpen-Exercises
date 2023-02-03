57de39540f1ae77db5b41cf1786a884801b26b7b
Meet <proobmeet7834@gmail.com>
2023-02-03 10:37:10 +0530

    Part 2 without exercise

--- src/App.js
+++ src/App.js
@@ -1,60 +1,82 @@
-import { useState } from "react";
-
-const Header = ({ text }) => <h1>{text}</h1>;
-
-const Anecdote = ({ text, votesCount }) => <div><p>Anecdotes: {text}</p><p>Votes: {votesCount}</p></div>
-
-const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
-
-const MostVoted = ({ anecdotes, allVotes }) => {
-    const higestScore = Math.max(...allVotes);
-    const mostVotedIndex = allVotes.indexOf(Math.max(...allVotes));
-    const mostVotedAnecdote = anecdotes[mostVotedIndex];
-    if (higestScore === 0) return <p>No Votes yet.</p>
-    return (
-        <div>
-            <p>Most Voted Anecdote: {mostVotedAnecdote}</p>
-            <p>Votes: {higestScore}</p>
-        </div>
-    )
-}
+import { useEffect, useState } from 'react';
+import Note from './components/Note'
+import Footer from './components/Footer';
+import Notification from './components/Notification';
+import noteService from './services/notes'
+import './index.css'
 
 const App = () => {
-    const anecdotes = [
-        'If it hurts, do it more often.',
-        'Adding manpower to a late software project makes it later!',
-        'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-        'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-        'Premature optimization is the root of all evil.',
-        'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-        'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-        'The only way to go fast, is to go well.'
-    ]
-    
-    const handleVoteClick = () => {
-        const newAllVotes = [...allVotes]
-        newAllVotes[selected] += 1
-        setAllVotes(newAllVotes)
+
+    const [notes, setNotes] = useState([]);
+    const [newNote, setNewNote] = useState('');
+    const [showAll, setShowAll] = useState(true);
+    const [errorMessage, setErrorMessage] = useState(null);
+
+    const hook = () => {
+        noteService
+            .getAll()
+            .then(res => {
+                setNotes(res);
+            })
     }
 
-    const handleAnecdoteClick = () => {
-        const arrayIndex = Math.floor(Math.random() * anecdotes.length)
-        setSelected(arrayIndex)
+    useEffect(hook, []);
+
+    const notesToShow = showAll ? notes : notes.filter(note => note.important);
+
+    const addNote = (event) => {
+        event.preventDefault();
+        const noteObject = {
+            content: newNote,
+            important: Math.random() < 0.5,
+        }
+        noteService
+            .create(noteObject)
+            .then(res => {
+                setNotes(notes.concat(res));
+                setNewNote('');
+            });
     }
 
-    const [selected, setSelected] = useState(0);
-    const [allVotes, setAllVotes] = useState(Array(8).fill(0))
+    const handleNoteChange = (event) => {
+        setNewNote(event.target.value);
+    }
+
+    const toggleImportance = (id) => {
+        const note = notes.find(n => n.id === id);
+        const changedNote = { ...note, important: !note.important };
+
+        noteService
+            .update(id, changedNote)
+            .then(res => {
+                setNotes(notes.map(n => n.id !== id ? n : res))
+            })
+            .catch(err => {
+                setErrorMessage(`Note '${note.content}' was already removed from the server.`);
+                setTimeout(()=>{
+                    setErrorMessage(null);
+                }, 5000)
+                setNotes(notes.filter(n => n.id !== id))
+            })
+    }
 
     return (
         <div>
-            <Header text='Ancedote of the day!' />
-            <Anecdote text={anecdotes[selected]} votesCount={allVotes[selected]} />
-
-            <Button text='Next' handleClick={handleAnecdoteClick} />
-            <Button text='Vote' handleClick={handleVoteClick} />
-
-            <Header text='Most voted Ancedote' />
-            <MostVoted anecdotes={anecdotes} allVotes={allVotes} />
+            <h1>Notes</h1>
+            <Notification message={errorMessage} />
+            <div>
+                <button onClick={() => setShowAll(!showAll)}>Show {showAll ? 'Important' : 'All'}</button>
+            </div>
+            <ul>
+                {notesToShow.map(note =>
+                    <Note key={note.id} note={note} toggleImportance={() => toggleImportance(note.id)} />
+                )}
+            </ul>
+            <form onSubmit={addNote}>
+                <input type="text" placeholder='Add Another Note' value={newNote} onChange={handleNoteChange} />
+                <button type='submit'>Submit</button>
+            </form>
+            <Footer />
         </div>
     )
 }
