c239c195707d28b6fa402f029a6cbbe2353d5419
Meet <proobmeet7834@gmail.com>
2023-07-21 17:18:40 +0530

    part 3 exercise 1-6

---
+++ exercises/Part 3/index.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json())
+
+let phonebook = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (req, res) => {
+    res.redirect("/api/persons");
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(phonebook);
+})
+
+app.get('/info', (req,res)=>{
+    res.send(`<p>Phonebook has info of ${phonebook.length} people.</p> <br/> <p>${new Date().toISOString()}</p>`)
+})
+
+app.get('/api/persons/:id', (req,res)=>{
+    const id = Number(req.params.id);
+    const person = phonebook.find(p=> p.id === id);
+    if (person) res.json(person);
+    else res.status(404).send("Person not found!");
+})
+
+app.delete('/api/persons/:id', (req,res)=>{
+    const id = Number(req.params.id);
+    phonebook = phonebook.filter(p=>p.id !== id);
+    res.status(200).send("Deletion succesfull");
+})
+
+app.post('/api/persons', (req,res)=>{
+    const body = req.body;
+    if (!body.name || !body.number) res.status(400).json({error: "Name or phone number not given."});
+
+    const isUnique = phonebook.find(p=>p.name === body.name) ? false : true;
+    if(!isUnique) res.status(409).json({error: "name must be unique"})
+
+    if (body.name && body.number && isUnique){
+        const person = {
+            id: (Math.floor(Math.random() *100) +5), //starting from 5 so that our current id aren't duplicated
+            name: body.name,
+            number: body.number
+        }
+        phonebook = phonebook.concat(person);
+        res.status(200).json("Person succesfully added.");
+    }
+})
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT: ${PORT}`);
+})
