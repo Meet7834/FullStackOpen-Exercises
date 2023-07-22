b78276cb4dcdb5636fc17502563c5bc8430a7c1c
Meet <proobmeet7834@gmail.com>
2023-07-22 10:37:23 +0530

    added backend class-work

--- src/services/notes.js
+++ src/services/notes.js
@@ -1,14 +1,9 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/notes'
+const baseUrl = 'http://localhost:3001/api/notes'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
-  const nonExisting = {
-    id: 10000,
-    content: 'this note does not exist',
-    important: true
-  }
-  return request.then(response => response.data.concat(nonExisting))
+  return request.then(response => response.data)
 }
 
 const create = newObject => {
@@ -21,5 +16,8 @@ const update = (id, newObject) => {
   return request.then(response => response.data)
 }
 
-const noteService = { getAll, create, update }
-export default noteService;
+export default { 
+  getAll, 
+  create, 
+  update 
+}
